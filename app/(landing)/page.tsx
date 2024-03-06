829a3c5dff23b25ba4bcb19161f7b460f92e7a81
Artur Gawron <artur@gawron.me>
2024-03-06 17:41:09 +0100

    WIP/landingPage

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,17 @@
+import { Speech } from 'lucide-react'
+
 const LandingPage = () => {
-    return <div>Landing page</div>
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center justify-center'>
+                <div className='text-b mb-4 flex items-center rounded-full border bg-amber-100 p-3 uppercase shadow-lg'>
+                    <Speech className='mr-4 h-10 w-10' />{' '}
+                    <p>Zamień tekst na mowę</p>
+                    <Speech className='ml-4 h-10 w-10 scale-x-[-1]' />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LandingPage
