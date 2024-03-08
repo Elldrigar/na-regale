55e4f6cf7210d4a3a65e9957a69e60daeca9b467
Artur Gawron <artur@gawron.me>
2024-03-08 23:02:42 +0100

    WIP/LandingPage project

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,11 +4,13 @@ const LandingPage = () => {
     return (
         <div className='flex flex-col items-center justify-center'>
             <div className='flex flex-col items-center justify-center'>
-                <div className='text-b mb-4 flex items-center rounded-full border bg-amber-100 p-3 uppercase shadow-lg'>
+                <div className='mb-4 flex items-center rounded-full border bg-amber-100 p-3 uppercase shadow-lg'>
                     <Speech className='mr-4 h-10 w-10' />{' '}
-                    <p>Zamień tekst na mowę</p>
+                    <p><span className='text-red-500 text-b'>AI</span> will speak to you</p>
                     <Speech className='ml-4 h-10 w-10 scale-x-[-1]' />
                 </div>
+                <h1 className='text-4xl md:test-6xl text center text-neutral-800 mb-6'>Wysłuchaj AI aj, aj ajjjj</h1>
+                <div className='text-3xl'></div>
             </div>
         </div>
     )
