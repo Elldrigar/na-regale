9805a1bad4e579542fdd1579da8ab403d88c8a72
Artur Gawron <artur@gawron.me>
2024-03-11 20:19:38 +0100

    Add email provider (passwordless)

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,11 +1,23 @@
 import type { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
+import EmailProvider from 'next-auth/providers/email'
 
 export const authOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string
+        }),
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD
+                }
+            },
+            from: process.env.EMAIL_FROM
         })
     ]
 } satisfies NextAuthOptions
