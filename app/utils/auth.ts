5a686dfdb7ccf1e3ba94cad30762477cda884b2d
Artur Gawron <artur@gawron.me>
2024-03-12 20:00:04 +0100

    Add Prisma adapter

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,8 +1,11 @@
 import type { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import EmailProvider from 'next-auth/providers/email'
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '@/app/utils/db'
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
