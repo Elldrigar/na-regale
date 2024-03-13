0a3249e7df4b92692e305adf9858c7aa0dd3b463
Artur Gawron <artur@gawron.me>
2024-03-13 21:47:16 +0100

    Fix type error on adapter

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -3,9 +3,10 @@ import GitHubProvider from 'next-auth/providers/github'
 import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from '@/app/utils/db'
+import type { Adapter } from 'next-auth/adapters'
 
 export const authOptions = {
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
