9116f163582f620393776581d6dea46efcc925a5
Artur Gawron <artur@gawron.me>
2024-03-20 21:00:29 +0100

    Landing - start here

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,30 @@
-export default function () {
-    return <h1>Hello, 100-commits!</h1>
+import { Button } from '@/components/ui/button'
+import { Roboto, Smooch_Sans } from 'next/font/google'
+import { cn } from '@/lib/utils'
+
+const font = Smooch_Sans({
+    weight: '200',
+    subsets: ['latin']
+})
+
+export default function Landing() {
+    return (
+        <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-700 to-violet-900'>
+            <div className='space-y-6 text-center'>
+                <h1
+                    className={cn(
+                        'text-8xl font-semibold text-white drop-shadow-md',
+                        font.className
+                    )}>
+                    Voice of Artificial Intelligence
+                </h1>
+                <p className='text-lg text-white'>During your meetings with the team</p>
+                <div>
+                    <Button size='lg' variant='secondary'>
+                       🤖 Start Here
+                    </Button>
+                </div>
+            </div>
+        </main>
+    )
 }
