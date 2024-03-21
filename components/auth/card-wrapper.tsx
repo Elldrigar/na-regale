d5deaad94f4976ea03ae2bd07d0564ef8bc50fc6
Artur Gawron <artur@gawron.me>
2024-03-21 22:15:45 +0100

    Card wrapper

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,22 +1,41 @@
 'use client'
-
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import React from 'react'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { Header } from '@/components/auth/card-header'
+import { SocialLogin } from '@/components/auth/card-socials-login'
+import { BackButton } from '@/components/auth/back-button'
 
 interface CardWrapperProps {
     children: React.ReactNode
     header: string
+    headerLabel: string
     backButtonLabel: string
     backButtonHref: string
     showSocial?: boolean
 }
 
-export const CordWrapper = ({
+export const CardWrapper = ({
     children,
     header,
+    headerLabel,
     backButtonLabel,
     backButtonHref,
     showSocial
 }: CardWrapperProps) => {
-    return <Card className='w-[500px] shadow-md'></Card>
+    return (
+        <Card className='w-[600px] shadow-xl'>
+            <CardHeader>
+                <Header header={header} label={headerLabel} />
+            </CardHeader>
+            <CardContent>{children}</CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <SocialLogin />
+                </CardFooter>
+            )}
+            <CardFooter className='justify-center'>
+                <BackButton href={backButtonHref} label={backButtonLabel} />
+            </CardFooter>
+        </Card>
+    )
 }
