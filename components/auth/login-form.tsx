fe1e07082a3df0b9180b87c561063f0ec765811d
Artur Gawron <artur@gawron.me>
2024-03-23 23:31:25 +0100

    Prepare resolvers

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,28 @@
+'use client'
+
+import * as z from "zod"
 import { CardWrapper } from '@/components/auth/card-wrapper'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { LoginSchema } from "@/schemas";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+    FormLabel
+} from '@/components/ui/form'
 
 export const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    })
+
     return (
         <CardWrapper
             header='Nazwa Aplikacji'
