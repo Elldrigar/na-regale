27106fbb3d39c7f1ef7f39bbb50073870018695c
Artur Gawron <artur@gawron.me>
2024-03-23 23:42:22 +0100

    WIP on Leogin inputs

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import * as z from "zod"
+import * as z from 'zod'
 import { CardWrapper } from '@/components/auth/card-wrapper'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { LoginSchema } from "@/schemas";
+import { LoginSchema } from '@/schemas'
 import {
     Form,
     FormControl,
@@ -13,14 +13,15 @@ import {
     FormMessage,
     FormLabel
 } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
 export const LoginForm = () => {
     const form = useForm<z.infer<typeof LoginSchema>>({
         resolver: zodResolver(LoginSchema),
         defaultValues: {
-            email: "",
-            password: ""
-        },
+            email: '',
+            password: ''
+        }
     })
 
     return (
@@ -30,7 +31,30 @@ export const LoginForm = () => {
             backButtonLabel='test'
             backButtonHref='test'
             showSocial>
-            Login
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(() => {})}
+                    className='space-y-6'>
+                    <div className='space-y-4'>
+                        <FormField
+                            name='email'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder='email@email.pl'
+                                            type='email'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                </form>
+            </Form>
         </CardWrapper>
     )
 }
