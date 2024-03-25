9871142591d014b2ba01f82e3ac49e02a038481e
Artur Gawron <artur@gawron.me>
2024-03-25 22:12:30 +0100

    Add schema for register

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,5 +6,17 @@ export const LoginSchema = z.object({
     }),
     password: z.string().min(1, {
         message: 'Hasło jest wymagane'
+    })
+})
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: 'Poprawny email jest wymagany'
     }),
+    password: z.string().min(6, {
+        message: 'Minimum 6 znaków'
+    }),
+    name: z.string().min(2, {
+        message: 'Imię jest wymagane'
+    })
 })
