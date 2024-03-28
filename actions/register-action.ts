2dede4abf78b41f0686caa36a9584a534d1c2463
Artur Gawron <artur@gawron.me>
2024-03-28 22:43:05 +0100

    Create user action

--- actions/register-action.ts
+++ actions/register-action.ts
@@ -1,14 +1,40 @@
 'use server'
 
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
 import { RegisterSchema } from '@/schemas'
+import { db } from '@/lib/db'
 
-export const registerAction = async (values: z.infer<typeof RegisterSchema>) => {
+export const registerAction = async (
+    values: z.infer<typeof RegisterSchema>
+) => {
     const validatedFields = RegisterSchema.safeParse(values)
+    const salt: number = 10
 
     if (!validatedFields.success) {
         return { error: 'Invalid fields' }
     }
 
-    return { success: "Success!" }
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const existingUserEmail = await db.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (existingUserEmail) {
+        return { error: 'Email is already taken!' }
+    }
+
+    await db.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: hashedPassword
+        }
+    })
+
+    return { success: 'Success!' }
 }
