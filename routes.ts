3884632e4492b75389e1c08bc85a1ac9211d1e40
Artur Gawron <artur@gawron.me>
2024-03-31 23:02:32 +0200

    Add routes

---
+++ routes.ts
@@ -0,0 +1,26 @@
+/**
+ *Arajka z rutami które będa publiczne
+ *Nie potrzebuja uwierzytelniania(authentication)
+ * @type {string[]}
+ */
+export const publicRoutes: string[] = ['/', '/auth/new-verification']
+
+/**
+ *Arajka z rutami które będą uzywane do uwierzytelniania(authentication)
+ *Te ruty przekierują zalogowanego użytkownika do /settings
+ * @type {string[]}
+ */
+export const authRoutes: string[] = ['/auth/login', '/auth/register']
+
+/**
+ * Prefix dla API authentication rutek
+ * Routy rozpoczynające się od tego prefiksu są używane do celów uwierzytelniania(authentication) API
+ * @type {string}
+ */
+export const apiAuthPrefix: string = '/api/auth'
+
+/**
+ * Defaultowa rutka po zalogowaniu się
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT: string = '/settings'
