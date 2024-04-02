e54f6fd88b6324b4f68d4b6df1bbf116f14387fb
Artur Gawron <artur@gawron.me>
2024-04-02 22:34:52 +0200

    WIP on login action

--- actions/login-action.ts
+++ actions/login-action.ts
@@ -2,6 +2,8 @@
 
 import * as z from 'zod'
 import { LoginSchema } from '@/schemas'
+import { signIn } from "@/auth";
+import {DEFAULT_LOGIN_REDIRECT} from "@/routes";
 
 export const loginAction = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values)
@@ -10,5 +12,13 @@ export const loginAction = async (values: z.infer<typeof LoginSchema>) => {
         return { error: 'Invalid fields' }
     }
 
-    return { success: "Success!" }
+    const { email, password } = validatedFields.data
+    try {
+        await signIn('credentials', {
+            email, password, redirectTo: DEFAULT_LOGIN_REDIRECT
+        })
+    } catch (err) {
+        //TODO
+    }
+
 }
