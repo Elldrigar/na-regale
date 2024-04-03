80ec71996406dca5524479068d428a1ab109455c
Artur Gawron <artur@gawron.me>
2024-04-03 22:36:37 +0200

    Add user Role to jwt token and session

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import authConfig from '@/auth.config'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { db } from '@/lib/db'
+import { getUserById } from '@/services/user/getUser'
 
 export const {
     handlers: { GET, POST },
@@ -14,10 +15,19 @@ export const {
             if (token.sub && session.user) {
                 session.user.id = token.sub
             }
+            if (token.role && session.user) {
+                session.user.role = token.role
+            }
             return session
         },
         async jwt({ token }) {
-            console.log({ token })
+            if (!token.sub) return token
+
+            const existingUser = await getUserById(token.sub)
+
+            if (!existingUser) return token
+
+            token.role = existingUser.role
             return token
         }
     },
