e3121fcf1ebd3ca7ce53fcb65c876b4f3c4cbd28
Artur Gawron <artur@gawron.me>
2024-04-03 20:38:54 +0200

    Add temporary logout button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,8 +1,19 @@
-import { auth } from '@/auth'
+import { auth, signOut } from '@/auth'
 
 const SettingsPage = async () => {
     const session = await auth()
-    return <div>{JSON.stringify(session)}</div>
+    return (
+        <div>
+            {JSON.stringify(session)}
+            <form
+                action={async () => {
+                    'use server'
+                    await signOut()
+                }}>
+                <button type='submit'>WYLOGUJ</button>
+            </form>
+        </div>
+    )
 }
 
 export default SettingsPage
