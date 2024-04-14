1903aeb45583ec896679cefbc85c2e304ec43182
Artur Gawron <artur@gawron.me>
2024-04-14 22:30:54 +0200

    add token for 2FA

--- services/user/getToken.ts
+++ services/user/getToken.ts
@@ -39,3 +39,23 @@ export const getPasswordResetTokenByEmail = async (email: string) => {
         return null
     }
 }
+
+export const getTwoFactorTokenByToken = async (token: string) => {
+    try {
+        return await db.twoFactorToken.findUnique({
+            where: {token}
+        })
+    } catch {
+        return null
+    }
+}
+
+export const getTwoFactorTokenByEmail = async (email: string) => {
+    try {
+        return await db.twoFactorToken.findFirst({
+            where: {email}
+        })
+    } catch {
+        return null
+    }
+}
