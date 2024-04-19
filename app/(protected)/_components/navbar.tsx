be79171c206e0ee90d2e8363e428cfdd90d00d2c
Artur Gawron <artur@gawron.me>
2024-04-19 23:20:16 +0200

    ad navbar to protected page

---
+++ app/(protected)/_components/navbar.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <div>NAVIGACJA!</div>
+        </nav>
+    )
+}
--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Navbar } from '@/app/(protected)/_components/navbar'
 
 interface ProtectedLayoutProps {
     children: React.ReactNode
@@ -7,6 +8,7 @@ interface ProtectedLayoutProps {
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
     return (
         <div className='flex h-full w-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-700 to-violet-900'>
+            <Navbar />
             {children}
         </div>
     )
