6a5ac48054b5b1b9202ba7330a7f31fed568aaf1
Artur Gawron <artur@gawron.me>
2024-04-21 15:42:47 +0200

    wip -on navbar

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,9 +1,21 @@
 'use client'
 
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+
 export const Navbar = () => {
+    const pathName = usePathname()
     return (
-        <nav>
-            <div>NAVIGACJA!</div>
+        <nav className='flex w-[800px] justify-between rounded-xl bg-secondary p-4 shadow-xl'>
+            <div>
+                <Button
+                    asChild
+                    variant={pathName === '/settings' ? 'default' : 'outline'}>
+                    <Link href='/settings'>Ustawienia</Link>
+                </Button>
+            </div>
+            <p>User</p>
         </nav>
     )
 }
