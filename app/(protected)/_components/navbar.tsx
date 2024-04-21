1e21a1c174101d3a6332470db0a306363145b4c1
Artur Gawron <artur@gawron.me>
2024-04-21 16:30:38 +0200

    add dropdown user button

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@/components/auth/user-button'
 
 export const Navbar = () => {
     const pathName = usePathname()
@@ -30,7 +31,7 @@ export const Navbar = () => {
                     <Link href='/browse'>Przeglądaj</Link>
                 </Button>
             </div>
-            <p>User</p>
+            <UserButton />
         </nav>
     )
 }
