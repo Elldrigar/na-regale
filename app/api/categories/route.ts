edf4a9b3447393da4ce02c5647598cef68368c13
Artur Gawron <artur@gawron.me>
2024-04-24 17:00:18 +0200

    add categiory GET api

---
+++ app/api/categories/route.ts
@@ -0,0 +1,15 @@
+import { db } from '@/lib/db'
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+    try {
+        const categories = await db.category.findMany();
+
+        return new NextResponse(JSON.stringify(categories, { status: 200 }));
+    } catch (err) {
+        console.log(err);
+        return new NextResponse(
+            JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+        );
+    }
+};
