ae38ee8bbf73942cd8aba780854b6fc99284dd85
Artur Gawron <artur@gawron.me>
2024-04-27 15:17:31 +0200

    add wrapper component to layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar } from '@/app/(protected)/_components/navbar'
+import { Main } from '@/components/ui/main'
 
 interface ProtectedLayoutProps {
     children: React.ReactNode
@@ -9,7 +10,7 @@ const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
     return (
         <div className='flex h-full w-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-700 to-violet-900'>
             <Navbar />
-            {children}
+            <Main>{children}</Main>
         </div>
     )
 }
