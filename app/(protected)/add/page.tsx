9b23dc5d5ffaf9694df1a9b43e32a710839c699a
Artur Gawron <artur@gawron.me>
2024-05-02 22:24:59 +0200

    testing user role gate

--- app/(protected)/add/page.tsx
+++ app/(protected)/add/page.tsx
@@ -1,11 +1,19 @@
+'use client'
 import { currentUser } from '@/lib/auth'
+import { UserRole } from '@prisma/client'
+import { RoleGate } from '@/components/auth/role-gate'
+import { FormSuccess } from '@/components/notification/form-success'
 
-const add = async () => {
-    const user = await currentUser()
+const add = () => {
     return (
         <div>
             <h1>DODAJ PAGE</h1>
-            <h2>{JSON.stringify(user)}</h2>
+            <RoleGate allowedRole={UserRole.ADMIN}>
+                <FormSuccess message='Jesteś Adminem! Enjoy the power!' />
+            </RoleGate>
+            <RoleGate allowedRole={UserRole.USER}>
+                <FormSuccess message='Jesteś Adminem! Enjoy the power!' />
+            </RoleGate>
         </div>
     )
 }
