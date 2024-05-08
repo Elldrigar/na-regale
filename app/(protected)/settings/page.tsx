a88b7f97fc23f148fdec40ac84c8ae93b4ff3f17
Artur Gawron <artur@gawron.me>
2024-05-08 23:45:14 +0200

    WIP on update

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -2,15 +2,27 @@
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
+import { settingsAction } from '@/actions/settings-action'
+import { useTransition } from 'react'
 
 const settings = () => {
+    const [isPending, startTransition] = useTransition()
+    const onClick = () => {
+        startTransition(() => {
+            settingsAction({
+                name: 'new name!'
+            })
+        })
+    }
     return (
         <Card className='w-[700px] shadow-md'>
             <CardHeader>
                 <p className='text-center text-2xl font-semibold'>USTAWIENIA</p>
             </CardHeader>
             <CardContent>
-                <Button>Update</Button>
+                <Button disabled={isPending} onClick={onClick}>
+                    Update
+                </Button>
             </CardContent>
         </Card>
     )
