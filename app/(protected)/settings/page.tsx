dd7eaf938e583bbdadee26afbeaf45d34ce1a2d1
Artur Gawron <artur@gawron.me>
2024-05-08 23:28:33 +0200

    WIP - Settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,18 @@
 'use client'
 
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+
 const settings = () => {
     return (
-        <div>
-            Settings
-        </div>
+        <Card className='w-[700px] shadow-md'>
+            <CardHeader>
+                <p className='text-center text-2xl font-semibold'>USTAWIENIA</p>
+            </CardHeader>
+            <CardContent>
+                <Button>Update</Button>
+            </CardContent>
+        </Card>
     )
 }
 
