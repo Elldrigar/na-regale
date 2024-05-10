b8c8a363b72a51d4baba569b22745c7ee66c1a5d
Artur Gawron <artur@gawron.me>
2024-05-10 23:00:02 +0200

    add isOAuth

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,6 +4,7 @@ import { type DefaultSession } from 'next-auth'
 export type ExtendedUser = DefaultSession['user'] & {
     role: UserRole
     is2FAEnabled: boolean
+    isOAuth: boolean
 }
 
 declare module 'next-auth' {
