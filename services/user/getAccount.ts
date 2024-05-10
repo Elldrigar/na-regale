3a42c9904852e08ea04143eb7cd8e249d2ffaa08
Artur Gawron <artur@gawron.me>
2024-05-10 22:54:59 +0200

    add get user account services

---
+++ services/user/getAccount.ts
@@ -0,0 +1,11 @@
+import {db} from '@/lib/db'
+
+export const getAccountByUserId = async (userId: string) => {
+    try {
+        return await db.account.findFirst({
+            where: {userId}
+        })
+    } catch {
+        return null
+    }
+}
