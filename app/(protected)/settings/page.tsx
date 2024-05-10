9f781ab937f5672dfc818cfc61f4d469a5a39b6e
Artur Gawron <artur@gawron.me>
2024-05-10 23:15:21 +0200

    WIP on form settings

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,28 +1,61 @@
 'use client'
 
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { SettingsSchema } from '@/schemas'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { settingsAction } from '@/actions/settings-action'
-import { useTransition } from 'react'
+import { useTransition, useState } from 'react'
+import { Input } from '@/components/ui/input'
+import {
+    Form,
+    FormField,
+    FormControl,
+    FormItem,
+    FormLabel,
+    FormDescription,
+    FormMessage
+} from '@/components/ui/form'
 
 const settings = () => {
+    const [success, setSuccess] = useState<string | undefined>()
+    const [error, setError] = useState<string | undefined>()
     const [isPending, startTransition] = useTransition()
-    const onClick = () => {
+    const form = useForm<z.infer<typeof SettingsSchema>>({
+        resolver: zodResolver(SettingsSchema),
+        defaultValues: {
+            name: ''
+        }
+    })
+
+    const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
         startTransition(() => {
-            settingsAction({
-                name: 'new name!'
-            })
+            settingsAction(values)
+                .then((data) => {
+                    if (data.error) {
+                        setError(data.error)
+                    }
+                    if (data.success) {
+                        setSuccess(data.success)
+                    }
+                })
+                .catch(() => setError('Coś poszło nie tak!'))
         })
     }
+
     return (
         <Card className='w-[700px] shadow-md'>
             <CardHeader>
                 <p className='text-center text-2xl font-semibold'>USTAWIENIA</p>
             </CardHeader>
             <CardContent>
-                <Button disabled={isPending} onClick={onClick}>
-                    Update
-                </Button>
+                <Form {...form}>
+                    <form
+                        className='space-y-6'
+                        onSubmit={form.handleSubmit(onSubmit)}></form>
+                </Form>
             </CardContent>
         </Card>
     )
