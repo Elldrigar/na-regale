df9e5b08b8ff9a65e29c223ef1355887f6251a8d
Artur Gawron <artur@gawron.me>
2024-05-23 23:35:22 +0200

    wip

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,16 +1,30 @@
-import { db } from '@/lib/db'
-import { currentUser } from '@/lib/auth'
+// import { db } from '@/lib/db'
+// import { currentUser } from '@/lib/auth'
+// import { NextResponse } from 'next/server'
+//
+// export async function POST(req: Request, res: Response) {
+//     const { title, desc } = req.json()
+//
+//     if (!title || !desc) {
+//         return NextResponse.json({ error: 'Missing title' }, { status: 500 })
+//     }
+//     const user = await currentUser()
+//     if (!user) {
+//         return NextResponse.json({ error: 'Brak usera!' }, { status: 401 })
+//     }
+//     const {} = await req.json()
+// }
+
+import { currentRole } from '@/lib/auth'
+import { UserRole } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
-export async function POST(req: Request, res: Response) {
-    const { title, desc } = req.json()
+export async function GET() {
+    const role = await currentRole()
 
-    if (!title || !desc) {
-        return NextResponse.json({ error: 'Missing title' }, { status: 500 })
+    if (role === UserRole.ADMIN) {
+        return new NextResponse(null, { status: 200 })
     }
-    const user = await currentUser()
-    if (!user) {
-        return NextResponse.json({ error: 'Brak usera!' }, { status: 401 })
-    }
-    const {} = await req.json()
+
+    return new NextResponse(null, { status: 403 })
 }
