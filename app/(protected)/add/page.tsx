262a4c09bb2017749992a4be5e741fde233ccccb
Artur Gawron <artur@gawron.me>
2024-05-26 23:34:25 +0200

    wip form

--- app/(protected)/add/page.tsx
+++ app/(protected)/add/page.tsx
@@ -5,6 +5,7 @@ import { FormSuccess } from '@/components/notification/form-success'
 import { Button } from '@/components/ui/button'
 import { toast } from 'sonner'
 import { adminActions } from '@/actions/admin-action'
+import { Form } from '@/components/ui/form'
 
 const add = () => {
     const onApiRouteClick = () => {
@@ -31,15 +32,16 @@ const add = () => {
     return (
         <div>
             <h1>DODAJ PAGE</h1>
-            <RoleGate allowedRole={UserRole.ADMIN}>
-                <FormSuccess message='Jesteś Adminem! Enjoy the power!' />
-            </RoleGate>
-            <RoleGate allowedRole={UserRole.USER}>
-                <FormSuccess message='Jesteś Adminem! Enjoy the power!' />
-            </RoleGate>
+            {/*<RoleGate allowedRole={UserRole.ADMIN}>*/}
+            {/*    <FormSuccess message='Jesteś Adminem! Enjoy the power!' />*/}
+            {/*</RoleGate>*/}
+            {/*<RoleGate allowedRole={UserRole.USER}>*/}
+            {/*    <FormSuccess message='Jesteś Adminem! Enjoy the power!' />*/}
+            {/*</RoleGate>*/}
 
-            <Button onClick={onApiRouteClick}>Testuj API</Button>
-            <Button onClick={onServerActionClick}>Testuj Server action</Button>
+            {/*<Button onClick={onApiRouteClick}>Testuj API</Button>*/}
+            {/*<Button onClick={onServerActionClick}>Testuj Server action</Button>*/}
+            <Form></Form>
         </div>
     )
 }
