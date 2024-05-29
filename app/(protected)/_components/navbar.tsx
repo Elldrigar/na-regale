4b3baa1e956e318805146e2cd10c7ed1d568d453
Artur Gawron <artur@gawron.me>
2024-05-29 22:22:00 +0200

    wip

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -5,34 +5,28 @@ import { usePathname } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { UserButton } from '@/components/auth/user-button'
 
+const links = [
+    { id: 1, label: 'Start', href: '/start' },
+    { id: 2, label: 'Czytaj', href: '/ai' },
+    { id: 3, label: 'Dodaj', href: '/add' },
+    { id: 4, label: 'Przeglądaj', href: '/browse' }
+]
+
 export const Navbar = () => {
     const pathName = usePathname()
     return (
         <nav className='flex w-[1000px] justify-between rounded-xl bg-secondary p-4 shadow-xl'>
             <div className='flex gap-x-2'>
-                <Button
-                    asChild
-                    variant={pathName === '/start' ? 'default' : 'outline'}>
-                    <Link href='/start'>Start</Link>
-                </Button>
-                <Button
-                    asChild
-                    variant={pathName === '/ai' ? 'default' : 'outline'}>
-                    <Link href='/ai'>
-                        Czyt
-                        <span className='font-bold text-fuchsia-700'>AI</span>
-                    </Link>
-                </Button>
-                <Button
-                    asChild
-                    variant={pathName === '/add' ? 'default' : 'outline'}>
-                    <Link href='/add'>Dodaj</Link>
-                </Button>
-                <Button
-                    asChild
-                    variant={pathName === '/browse' ? 'default' : 'outline'}>
-                    <Link href='/browse'>Przeglądaj</Link>
-                </Button>
+                {links.map((link) => (
+                    <Button
+                        key={link.id}
+                        asChild
+                        variant={
+                            pathName === `${link.href}` ? 'default' : 'outline'
+                        }>
+                        <Link href={link.href}>{link.label}</Link>
+                    </Button>
+                ))}
             </div>
             <UserButton />
         </nav>
